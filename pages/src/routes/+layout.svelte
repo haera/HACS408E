d9b80779186a2906e94dbe08554b239d618958e3
Luke Mains <past.hat6941@fastmail.com>
2024-06-26 19:02:26 -0400

    Fix Navigation Bar

---
+++ pages/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<nav>
+  <ul class="horizontal">
+    <li><a href="/">HACS408E</a></li>
+    <li><a href="/lectures">Lectures</a></li>
+  </ul>
+</nav>
+
+<slot />
+
+<style>
+  ul.horizontal {
+    display: flex;
+    list-style-type: none;
+    margin: 0;
+    padding-left: 0;
+  }
+  li {
+    padding-right: 10px;
+  }
+</style>
